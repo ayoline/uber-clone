4efe664a4b7d62489dfef5c5a2aff4e83732e827
ayoline <ayo@outlook.com.br>
2021-09-07 22:40:05 -0300

    Configura a interface de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'telas/Home.dart';
+
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff37474f),
+  accentColor: Color(0xff546e7a),
+);
 
 void main() async {
   //await Firebase.initializeApp();
@@ -10,5 +14,6 @@ void main() async {
     debugShowCheckedModeBanner: false,
     title: "Uber",
     home: Home(),
+    theme: temaPadrao,
   ));
 }
